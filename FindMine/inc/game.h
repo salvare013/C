677df53290ad62cd0a83ac827dd3a816f648a41c
salvare013 <salvare00013@foxmail.com>
2024-06-21 14:18:18 +0800

    modify format

--- FindMine/inc/game.h
+++ FindMine/inc/game.h
@@ -1,24 +1,34 @@
 #pragma once
 
-//定义常量
+// 定义常量
 #define ROW 9
 #define COL 9
-#define ROWS ROW+2
-#define COLS COL+2
+#define ROWS ROW + 2
+#define COLS COL + 2
 #define EASY_COUNT 13
 
-//包含库函数
+// 包含库函数
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-//函数声明
+// 函数声明
 
-void InitBoard(char board[ROWS][COLS], int rows, int cols, char set);
-void DisplayBoard(char board[ROWS][COLS], int row, int col);
-void SetMine(char mine[ROWS][COLS], int row, int col);
+void InitBoard(char board[ROWS][COLS],
+               int rows,
+               int cols,
+               char set);
+void DisplayBoard(char board[ROWS][COLS],
+                  int row,
+                  int col);
+void SetMine(char mine[ROWS][COLS],
+             int row,
+             int col);
 
-//返回两种游戏状态
-//失败	'1'
-//成功	'W'
-char FindMine(char mine[ROWS][COLS], char show[ROWS][COLS], int row, int col);
+// 返回两种游戏状态
+// 失败	'1'
+// 成功	'W'
+char FindMine(char mine[ROWS][COLS],
+              char show[ROWS][COLS],
+              int row,
+              int col);
--- FindMine/src/test.c
+++ FindMine/src/test.c
@@ -1,62 +1,64 @@
 #include "game.h"
 
 void Menu() {
-	printf("************************************\n");
-	printf("*******      1.play       **********\n");
-	printf("*******      0.exit       **********\n");
-	printf("************************************\n");
-
+  printf(
+      "************************************\n");
+  printf(
+      "*******      1.play       **********\n");
+  printf(
+      "*******      0.exit       **********\n");
+  printf(
+      "************************************\n");
 }
 void Game() {
-	//判断游戏状态
-	char ret = 0;
-	//储存布置的雷信息
-	char mine[ROWS][COLS] = { 0 };
-	//储存排除的雷信息
-	char show[ROWS][COLS] = { 0 };
-	//初始化
-	InitBoard(mine, ROWS, COLS, '0');
-	InitBoard(show, ROWS, COLS, '*');
-	//布置雷
-	SetMine(mine, ROW, COL);
-	DisplayBoard(mine, ROW, COL);
-	DisplayBoard(show, ROW, COL);
-	//扫雷
-	ret = FindMine(mine, show, ROW, COL);
-	if (ret == '1') {
-		printf("很遗憾,你被炸死了!\n");
-		DisplayBoard(mine, ROW, COL);
-		printf("GAME OVER!\n");
+  // 判断游戏状态
+  char ret = 0;
+  // 储存布置的雷信息
+  char mine[ROWS][COLS] = {0};
+  // 储存排除的雷信息
+  char show[ROWS][COLS] = {0};
+  // 初始化
+  InitBoard(mine, ROWS, COLS, '0');
+  InitBoard(show, ROWS, COLS, '*');
+  // 布置雷
+  SetMine(mine, ROW, COL);
+  DisplayBoard(mine, ROW, COL);
+  DisplayBoard(show, ROW, COL);
+  // 扫雷
+  ret = FindMine(mine, show, ROW, COL);
+  if (ret == '1') {
+    printf("很遗憾,你被炸死了!\n");
+    DisplayBoard(mine, ROW, COL);
+    printf("GAME OVER!\n");
 
-	} else {
-		printf("哇!!! 恭喜你排雷成功了!\n");
-		DisplayBoard(mine, ROW, COL);
-		printf("你是我的YYDS!\n");
-	}
-
+  } else {
+    printf("哇!!! 恭喜你排雷成功了!\n");
+    DisplayBoard(mine, ROW, COL);
+    printf("你是我的YYDS!\n");
+  }
 }
 void test() {
-	int input = 0;
-	srand((unsigned int)time(NULL));
-	do {
-		Menu();
-		printf("请选择:>");
-		scanf("%d", &input);
-		switch (input) {
-		case 1:
-			Game();
-			break;
-		case 0:
-			printf("退出游戏!\n");
-			break;
-		default:
-			printf("选择错误,请重新选择!\n");
-			break;
-		}
-	} while (input);
+  int input = 0;
+  srand((unsigned int)time(NULL));
+  do {
+    Menu();
+    printf("请选择:>");
+    scanf("%d", &input);
+    switch (input) {
+      case 1:
+        Game();
+        break;
+      case 0:
+        printf("退出游戏!\n");
+        break;
+      default:
+        printf("选择错误,请重新选择!\n");
+        break;
+    }
+  } while (input);
 }
 int main() {
-	test();
+  test();
 
-	return 0;
+  return 0;
 }
