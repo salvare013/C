e3d04e1a7154fcc5a17280ba5aee12848c1cb480
salvare013 <salvare00013@foxmail.com>
2024-07-15 22:11:16 +0800

    update

--- string/inc/str.h
+++ string/inc/str.h
@@ -1,26 +1,19 @@
 #pragma once
 #include <assert.h>
-#define NULL ((void *)0)
+#include <stdio.h>
+
 int StrLen(const char *str);
-char *StrCat(char *dest,
-             const char *src,
-             size_t destSize);
+char *StrCat(char *dest, const char *src, size_t destSize);
 int StrCmp(const char *str1, const char *str2);
-char *StrCpy(char *dest,
-             const char *src,
-             size_t destSize);
+char *StrCpy(char *dest, const char *src, size_t destSize);
 char *StrNCat(char *front,
               const char *back,
               unsigned int count,
               size_t frontSize);
-int StrNCmp(const char *str1,
-            const char *str2,
-            unsigned int count);
+int StrNCmp(const char *str1, const char *str2, unsigned int count);
 char *StrNCpy(char *dest,
               const char *src,
               unsigned int count,
               size_t destSize);
-const char *StrStr(const char *supStr,
-                   const char *subStr);
-int StrNStr(const char *supStr,
-            const char *subStr);
+const char *StrStr(const char *supStr, const char *subStr);
+int StrNStr(const char *supStr, const char *subStr);
--- string/main.c
+++ string/main.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "str.h"
 
 #define SZ(X) sizeof((X))
