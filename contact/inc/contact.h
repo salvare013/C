c5f48c14ca10c1adaf38fc1f0000b6650349b5cf
salvare013 <salvare00013@foxmail.com>
2024-07-30 13:28:25 +0800

    add contact and change dirName

---
+++ contact/inc/contact.h
@@ -0,0 +1,49 @@
+#pragma once
+
+// 包含库函数
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// 宏
+#define MALLOC(TYPE, SIZE) ((TYPE *)malloc(SIZE * sizeof(TYPE)))
+#define REALLOC(TYPE, PTR, NEW_SIZE)                                           \
+  ((TYPE *)realloc(PTR, (NEW_SIZE) * sizeof(TYPE)))
+
+// 定义常量
+#define DEFAULT_SZ 2
+#define MAX_NAME 20
+#define MAX_SEX 13
+#define MAX_TELE 12
+#define MAX_ADDR 50
+#define MAX_QQ 11
+
+// 定义类型
+enum Option { EXIT, ADD, DEL, SEARCH, MODIFY, SHOW, SORT, SAVE };
+enum ModifyOption { EXIT_M, NAME, AGE, SEX, TELE, QQ, ADDR };
+typedef struct Information {
+  char name[MAX_NAME];
+  int age;
+  char sex[MAX_SEX];
+  char tele[MAX_TELE];
+  char qq[MAX_QQ];
+  char addr[MAX_ADDR];
+} Information;
+typedef struct Contact {
+  struct Information *data;
+  int size;
+  int capacity;
+} Contact;
+
+// 声明函数
+void InitContact(struct Contact *ps);
+void AddCon(struct Contact *ps);
+void ShowCon(const struct Contact *ps);
+void DelCon(struct Contact *ps);
+void SearchCon(const struct Contact *ps);
+void ModifyCon(struct Contact *ps);
+void SortCon(struct Contact *ps);
+void DestroyCon(Contact *ps);
+void SaveCon(Contact *ps);
+int CheckCapacity(Contact *ps);
---
+++ contact/main.c
@@ -0,0 +1,59 @@
+#include "contact.h"
+
+static void Menu() {
+  printf("*************************************\n");
+  printf("***      1.add      2.del         ***\n");
+  printf("***      3.search   4.modify      ***\n");
+  printf("***      5.show     6.sort        ***\n");
+  printf("***      7.save     0.exit        ***\n");
+  printf("*************************************\n");
+}
+int main() {
+  int input = 0;
+  struct Contact con;
+  InitContact(&con);
+  do {
+    Menu();
+    printf("请选择:>");
+    scanf("%d", &input);
+    switch (input) {
+    case EXIT: {
+      int choose = 0;
+      printf("是否保存?\n");
+      printf("请选择:\nYes(1) or No(0)\t>");
+      scanf("%d", &choose);
+      if (choose == 1) {
+        SaveCon(&con);
+      }
+      DestroyCon(&con);
+      printf("退出!\n");
+    } break;
+    case ADD:
+      AddCon(&con);
+      break;
+    case DEL:
+      DelCon(&con);
+      break;
+    case SEARCH:
+      SearchCon(&con);
+      break;
+    case MODIFY:
+      ModifyCon(&con);
+      break;
+    case SHOW:
+      ShowCon(&con);
+      break;
+    case SORT:
+      SortCon(&con);
+      break;
+    case SAVE:
+      SaveCon(&con);
+      break;
+    default:
+      printf("选择错误!\n");
+      break;
+    }
+  } while (input);
+
+  return 0;
+}
